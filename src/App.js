f41810aa63db5baa7e504e47cccfc6cff8081683
ulfatun_amanah <ulfaamana@gmail.com>
2023-01-29 11:40:44 +0700

    technical-test-nds

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+/* eslint-disable no-loop-func */
+/* eslint-disable array-callback-return */
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [words, setWords] = React.useState("");
+  const [families, setFamilies] = React.useState("");
+  const [familyMembers, setFamilyMembers] = React.useState("");
+  const vowels = ["a", "i", "u", "e", "o"];
+
+  const checkLetter = () => {
+    let newWords = words.toLowerCase().split(" ").join("").split("");
+    let vowelsResult = ""
+    let consonantsResult = ""
+    for(let i=0; i < newWords.length; i++){
+      if (vowels.indexOf(newWords[i]) >= 0) {
+        vowelsResult += newWords[i];
+      } else {
+        consonantsResult += newWords[i];
+      }
+    }
+    document.getElementById("vowels-test").innerHTML = "Vowels: " + vowelsResult;
+    document.getElementById("consonant-test").innerHTML = "Consonant: " + consonantsResult;
+  }
+
+  const checkBusMinimum = () => {
+    let numbers = familyMembers.split(" ").join("").split("");
+    if(families > numbers.length){
+      document.getElementById("test-2").innerHTML = "Input must be equal with count of family";
+    } else {
+      let busTotal = 0;
+      let tempData = familyMembers.split(" ").join("").split("");
+      numbers.map((number) => {
+        let newNumber = parseInt(number);
+        let index = tempData.findIndex((number) => parseInt(number) + newNumber <= 4)
+        tempData.splice(index, 1)
+        tempData.splice(tempData.indexOf(newNumber), 1)
+        busTotal++
+      })
+      document.getElementById("test-2").innerHTML = `Minimum bus required is : ${busTotal}`;
+    }
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h6>Test 1</h6>
+        <p id="vowels-test"></p>
+        <p id="consonant-test"></p>
+        <input
+          type="text"
+          value={words}
+          onChange={(e) => setWords(e.target.value)}
+          placeholder="Masukkan Kata"
+        />
+        <button onClick={checkLetter}>Check</button>
+      </div>
+      <div>
+        <h6>Test 2</h6>
+        <p id="test-2"></p>
+        <input
+          type="number"
+          value={families}
+          onChange={(e) => setFamilies(e.target.value)}
+          placeholder="Masukkan Jumlah Keluarga"
+        />
+        <input
+          type="text"
+          value={familyMembers}
+          onChange={(e) => setFamilyMembers(e.target.value)}
+          placeholder="Masukkan Jumlah Anggota Keluarga"
+        />
+        <button onClick={checkBusMinimum}>Check</button>
+      </div>
     </div>
   );
 }
